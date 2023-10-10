6fcd93b2e97611261282305b40b52d5b20d131f4
sierrawebid <sierrawebid@gmail.com>
2023-10-10 20:02:10 +0800

    Update

--- src/routes/(app)/+page.server.js
+++ src/routes/(app)/+page.server.js
@@ -8,7 +8,7 @@ export const load = async () => {
 	try {
 		const [res1, res2] = await Promise.all([
 			fetch(`${API_ENDPOINT}/get/patroli/get-note-patroli`).then((res) => res.json()),
-			fetch(`${API_ENDPOINT}/get/presensi/get-piket/${piket}-${now}`).then((res) => res.json())
+			fetch(`${API_ENDPOINT}/get/presensi/get-piket/${piket}_${now}`).then((res) => res.json())
 		]);
 
 		return { data1: res1, data2: res2 };
